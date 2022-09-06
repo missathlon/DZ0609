dcfb83516b674fd4c91bd08d1997357cccc65d06
Ekaterina <missathlon08@gmail.com>
2022-09-06 16:32:44 +0300

    add Task 23

---
+++ task23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 23
+
+// Напишите программу, 
+// которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Vvedite chislo N = ");
+int N = Convert.ToInt16(Console.ReadLine());
+int i = 1;
+for (i = 1; i <= N; i++)
+    Console.WriteLine(i + "^3 = " + i * i * i);
